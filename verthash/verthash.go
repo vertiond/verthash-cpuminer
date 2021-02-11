818380406df00d9400c6cb93eb328301ebab2ac2
vertiond <35286924+vertiond@users.noreply.github.com>
2021-02-11 11:08:58 -0600

    Fix problems in file layout of verthash.dat

--- verthash/verthash.go
+++ verthash/verthash.go
@@ -2,8 +2,12 @@ package verthash
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/binary"
+	"encoding/hex"
+	"fmt"
 	"io/ioutil"
+	"os"
 
 	"golang.org/x/crypto/sha3"
 )
@@ -17,36 +21,109 @@ const VerthashRotations uint32 = 32
 const VerthashIndexes uint32 = 4096
 const VerthashByteAlignment uint32 = 16
 
-type Verthasher struct {
-	datafile []byte
+func EnsureVerthashDatafile(file string) error {
+	return EnsureVerthashDatafileWithProgress(file, nil)
 }
 
-func NewVerthasher(dataFileLocation string) (*Verthasher, error) {
-	b, err := ioutil.ReadFile(dataFileLocation)
-	if err != nil {
-		return nil, err
+func EnsureVerthashDatafileWithProgress(file string, progress chan float64) error {
+	MakeVerthashDatafileIfNotExistsWithProgress(file, progress)
+	ok, err := VerifyVerthashDatafile(file)
+	if err != nil || !ok {
+		os.Remove(file)
+		MakeVerthashDatafileWithProgress(file, progress)
+		ok, err := VerifyVerthashDatafile(file)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return fmt.Errorf("Could not crate or verify Verthash file")
+		}
 	}
 
-	return &Verthasher{datafile: b}, nil
+	return nil
+
+}
+
+func MakeVerthashDatafileIfNotExists(file string) error {
+	return MakeVerthashDatafileIfNotExistsWithProgress(file, nil)
+}
+
+func MakeVerthashDatafileIfNotExistsWithProgress(file string, progress chan float64) error {
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return MakeVerthashDatafileWithProgress(file, progress)
+	}
+	return nil
+}
+
+func MakeVerthashDatafile(file string) error {
+	return MakeVerthashDatafileWithProgress(file, nil)
+}
+
+func MakeVerthashDatafileWithProgress(file string, progress chan float64) error {
+	pk := sha3.Sum256([]byte("Verthash Proof-of-Space Datafile"))
+
+	NewGraph(17, file, pk[:], progress)
+
+	return nil
+}
+
+func VerifyVerthashDatafile(file string) (bool, error) {
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		return false, err
+	}
+	hash := sha256.Sum256(b)
+	expectedHash, _ := hex.DecodeString("a55531e843cd56b010114aaf6325b0d529ecf88f8ad47639b6ededafd721aa48")
+	if !bytes.Equal(hash[:], expectedHash) {
+		return false, fmt.Errorf("Generated file has wrong hash: %x vs %x", hash, expectedHash)
+	}
+	return true, nil
+}
+
+type Verthash struct {
+	datFileContent []byte
+	datFile        *os.File
 }
 
 func fnv1a(a, b uint32) uint32 {
 	return (a ^ b) * 0x1000193
 }
 
-func (vh *Verthasher) Hash(input []byte) [32]byte {
+func NewVerthash(datFileLocation string, keepInRam bool) (*Verthash, error) {
+	v := Verthash{}
+	var err error
+	if keepInRam {
+		v.datFileContent, err = ioutil.ReadFile(datFileLocation)
+	} else {
+		v.datFile, err = os.Open(datFileLocation)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &v, nil
+}
+
+func (v *Verthash) Close() {
+	if v.datFileContent != nil {
+		v.datFileContent = []byte{}
+	} else {
+		v.datFile.Close()
+	}
+}
+
+func (v *Verthash) SumVerthash(input []byte) ([32]byte, error) {
 	p1 := [32]byte{}
 
-	input_copy := make([]byte, len(input))
-	copy(input_copy[:], input[:])
-
-	sha3hash := sha3.Sum256(input_copy)
+	inputCopy := make([]byte, len(input))
+	copy(inputCopy[:], input[:])
+	sha3hash := sha3.Sum256(inputCopy)
 
 	copy(p1[:], sha3hash[:])
 	p0 := make([]byte, VerthashSubset)
 	for i := uint32(0); i < VerthashIter; i++ {
-		input_copy[0] += 0x01
-		digest64 := sha3.Sum512(input_copy)
+		inputCopy[0] += 0x01
+		digest64 := sha3.Sum512(inputCopy)
 		copy(p0[i*VerthashP0Size:], digest64[:])
 	}
 
@@ -54,7 +131,6 @@ func (vh *Verthasher) Hash(input []byte) [32]byte {
 	p0Index := make([]uint32, len(p0)/4)
 	for i := 0; i < len(p0Index); i++ {
 		binary.Read(buf, binary.LittleEndian, &p0Index[i])
-
 	}
 
 	seekIndexes := make([]uint32, VerthashIndexes)
@@ -66,9 +142,21 @@ func (vh *Verthasher) Hash(input []byte) [32]byte {
 		}
 	}
 
+	var datFileSize int64
+
+	if v.datFileContent == nil {
+		s, err := v.datFile.Stat()
+		if err != nil {
+			return [32]byte{}, err
+		}
+		datFileSize = s.Size()
+	} else {
+		datFileSize = int64(len(v.datFileContent))
+	}
+
 	var valueAccumulator uint32
 	var mdiv uint32
-	mdiv = ((uint32(len(vh.datafile)) - VerthashHashOutSize) / VerthashByteAlignment) + 1
+	mdiv = ((uint32(datFileSize) - VerthashHashOutSize) / VerthashByteAlignment) + 1
 	valueAccumulator = uint32(0x811c9dc5)
 	buf = bytes.NewBuffer(p1[:])
 	p1Arr := make([]uint32, VerthashHashOutSize/4)
@@ -77,8 +165,17 @@ func (vh *Verthasher) Hash(input []byte) [32]byte {
 	}
 	for i := uint32(0); i < VerthashIndexes; i++ {
 		offset := (fnv1a(seekIndexes[i], valueAccumulator) % mdiv) * VerthashByteAlignment
+
+		data := make([]byte, 32)
+		if v.datFileContent != nil {
+			data = v.datFileContent[offset : offset+VerthashHashOutSize]
+		} else {
+			v.datFile.Seek(int64(offset), 0)
+			v.datFile.Read(data)
+		}
+
 		for i2 := uint32(0); i2 < VerthashHashOutSize/4; i2++ {
-			value := binary.LittleEndian.Uint32(vh.datafile[offset+i2*4 : offset+((i2+1)*4)])
+			value := binary.LittleEndian.Uint32(data[i2*4 : ((i2 + 1) * 4)])
 			p1Arr[i2] = fnv1a(p1Arr[i2], value)
 			valueAccumulator = fnv1a(valueAccumulator, value)
 		}
@@ -88,5 +185,5 @@ func (vh *Verthasher) Hash(input []byte) [32]byte {
 		binary.LittleEndian.PutUint32(p1[i*4:], p1Arr[i])
 	}
 
-	return p1
+	return p1, nil
 }
